7fdce42c385bfcf082977af580aea78c406a3278
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2026-02-21 10:02:17 -0600

    feat: hover gradient effect (#6)

--- components/GradientText.tsx
+++ components/GradientText.tsx
@@ -1,24 +1,55 @@
+import { useRef } from "react";
 import { Box } from "@mui/material";
-import type { ReactNode } from "react";
+import type { ReactNode, PointerEvent } from "react";
 
 interface GradientTextProps {
   children: ReactNode;
   gradient?: string;
+  makeLettersBiggerOnHover?: boolean
 }
 
+/*====================================================
+* How could a site ever be cool without gradient text?
+* 
+* You'll notice on mouseover we do a cool little effect.
+* It moves the gradient around. It's meant to be cute.
+* 
+* We use a css var to do so instead of say, useState.
+* 
+* This works better than trying to change the property
+* with state, so we avoid re-renders. We like to 
+* avoid those. Also it performs well and is inexpensive.
+*======================================================*/
 export default function GradientText({
   children,
   gradient = "linear-gradient(45deg, #fff8cc, #ffc05c, #ffb7d6, #f1ccff, #c6fff6, #fff8cc)",
 }: GradientTextProps) {
+  const $Box = useRef<HTMLDivElement>(null);
+
+  function handlePointerMove(e: PointerEvent<HTMLDivElement>) {
+    const $BoxWithMouse = $Box.current;
+    const rect = e.currentTarget.getBoundingClientRect();
+    const posX = e.clientX - rect.left;
+
+    $BoxWithMouse!.style.setProperty("--mouseX", `${posX}px`);
+  }
+
   return (
     <Box
+      ref={$Box}
+      onPointerMove={handlePointerMove}
       sx={{
+        "--mouseX": "0",
         display: "inline-block",
         backgroundImage: gradient,
+        backgroundRepeat: 'repeat',
+        backgroundPositionX: `var(--mouseX)`,
         backgroundClip: "text",
         WebkitBackgroundClip: "text",
         color: "transparent",
         WebkitTextFillColor: "transparent",
+        textShadow: 'none',
+        cursor: 'default',
 
         "& > *": {
           display: "contents",
@@ -27,5 +58,5 @@ export default function GradientText({
     >
       {children}
     </Box>
-  );
+  )
 }
