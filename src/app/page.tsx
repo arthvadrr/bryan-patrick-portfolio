b9f0fcaf64dfc2c5e23fb1107a6742b0af9f1c8a
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2026-02-21 01:01:55 -0600

    Feat/animated arth and fancy text wrapper (#5)
    * feat: animated arth
    
    * feat: gradient text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Stack from "@mui/material/Stack";
 import FeaturedMemos from "../../components/FeaturedMemos";
 import FeaturedBoilerPlates from "../../components/FeaturedBoilerplates";
 import FeaturedSnippets from "../../components/FeaturedSnippets";
+import GradientText from "../../components/GradientText";
 
 /*============================================================
  * "The place where you lock yourself in and lock all else out, 
@@ -26,17 +27,17 @@ export default function Home() {
         alignItems="center"
         sx={{
           alignItems: 'center',
-          gap: 2,
+          gap: 4,
         }}>
         <Image
-          src="/images/pixel-bryan_128x128.png"
+          src="/images/arth-animated.gif"
           alt="Pixel Bryan"
-          width="140"
-          height="140"
+          width="128"
+          height="220"
         />
         <Stack spacing={2}>
           <Typography variant="h1">
-            Hello! <br /> My name is Bryan.
+            Hello! <br /> My name is <GradientText>Bryan</GradientText>.
           </Typography>
           <Typography>
             This is my personal site. Here there are code snippets, a collection of memos, boilerplates, and widgets for things I use all the time.
