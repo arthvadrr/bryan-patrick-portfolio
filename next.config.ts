d2522c83662fd5db7b782c2e9579fb9d1bbc5e1d
Bryan <54081465+arthvadrr@users.noreply.github.com>
2026-02-21 20:41:06 -0600

    feat & chore: configuration and mdx setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+import createMDX from '@next/mdx';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	reactCompiler: true,
+	pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+	options: {
+		rehypePlugins: [
+			[
+				'rehype-pretty-code',
+				{
+					theme: 'github-dark-dimmed',
+					keepBackground: false,
+				},
+			],
+		],
+	},
+});
+
+export default withMDX(nextConfig);
